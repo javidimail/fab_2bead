9ab15a4288909cb0e6ea8f71e3ea809d6bd1a271
javidimail <javidimail@gmail.com>
2014-03-26 17:44:45 -0400

    updated

---
+++ out.py
@@ -0,0 +1,22 @@
+Local current time : Fri Mar 21 13:11:25 2014 
+Computar host name: master 
+User: javidi 
+
+Read atoms of type CA : 93 Out of 181
+Read atoms of type CB : 88 Out of 181
+
+Converting residue types to codes for THR
+Total number of converted residues to codes: 93
+Nomber of converted residues to code: 
+0-GLY: 5 
+1-ALA: 4 
+2-SER: 16 
+3-CYS 2
+
+Converting residue types to codes for THR
+Total number of converted residues to codes: 88
+Nomber of converted residues to code: 
+0-GLY: 0 
+1-ALA: 4 
+2-SER: 16 
+3-CYS 2
---
+++ script2.py
@@ -0,0 +1,3 @@
+b = open("ch1_ca_cb.cor", "r").readlines()
+line = b[-2].split() #Gets the second last value from the list and split on whitespace
+print line
